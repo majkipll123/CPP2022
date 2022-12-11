aaf2a3b74b2559ebc48817158e3e0a1fb85b2951
majkipll123 <115625416+majkipll123@users.noreply.github.com>
2022-12-11 11:44:13 +0100

    snake FIN

--- pwpr/lista5/snake2.cpp
+++ pwpr/lista5/snake2.cpp
@@ -1,5 +1,8 @@
-#include <Windows.h>
-#include <unistd.h>
+#include <chrono>
+#include <thread>//dwie biblioteki do dluzszego sleepa
+
+#include <Windows.h> //zakomentuj na linux
+#include <unistd.h> //<stdio.h>
 #include <conio.h> //console input output
 #include <iostream>
 using namespace std;
@@ -7,6 +10,8 @@ bool koniecgry;
 const int width = 20;
 const int height = 20;
 int x, y, ox, oy, punkty;
+int tailX[100], tailY[100];
+int No;
 enum eDirection {STOP = 0, LEFT , RIGHT, UP, DOWN}dir;
 
 void setup()
@@ -22,7 +27,7 @@ void setup()
 void rysuj()
 {
     system("cls");//system("clear");
-    cout<<"witaj w prostym snake!"<<endl;
+    cout<<"witaj w prostym snake Karola!"<<endl;
     for (int i=0; i<width+1; i++)
         cout << "=";
     cout<<endl;
@@ -34,11 +39,29 @@ void rysuj()
             if (j==0)
                 cout<<"|";
             if (i == y&& j==x )
-                cout<<"%";
+                cout<<"\x1b[102;90m%\x1b[0m";
             else if (i==oy && j== ox)
-                cout<<"$";
+                cout<<"\x1B[101m$\x1b[0m";
+//\x1b[106m cyan
+//\x1b[102m green
+//\x1B[101m red
+//\x1b[0m reset
             else
-                cout<<" ";
+            {
+                bool print=false;
+                for (int k=0; k<No; k++)
+                {
+
+                   if (tailX[k]==j && tailY[k] ==i)
+                        {
+                        cout<<"\x1b[102;90mo\x1b[0m";
+                        print= true;
+                        }
+
+                }
+                if (!print)
+                    cout<<"\x1b[107m \x1b[0m";
+            }
             if (j==width-1)
                 cout<<"|";
         }
@@ -76,6 +99,21 @@ void input()
 }
 void logika()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0]=x;
+    tailY[0]=y;
+    for (int i =1; i< No; i++)
+    {
+        prev2X= tailX[i];
+        prev2Y=tailY[i];
+        tailX[i]=prevX;
+        tailY[i]=prevY;
+        prevX=prev2X;
+        prevY=prev2Y;
+
+    }
     switch (dir)
     {
     case UP:
@@ -93,17 +131,22 @@ void logika()
     default:
         break;
     }
-    if (x>width || x<0 ||y >height||y<0)
+    if (x>width || x<0 ||y >height||y<0) //sprawdza czy nie uderzyles w sciane
         koniecgry=true;
+    for (int i=0; i<No; i++) // sprawdza czy nie uderzyles w siebie
+    if(tailX[i]==x&&tailY[i]==y)
+    koniecgry=true;
+
     if (x==ox && y==oy)
     {
+
         punkty+=10;
         ox=rand()%width;
         oy=rand()%height;
+        No++;
     }
 }
 int main()
-
 {
     setup();
     while(!koniecgry)
@@ -111,7 +154,8 @@ int main()
         rysuj();
         input();
         logika();
-        sleep(1);
+        chrono::milliseconds timespan(90);
+        this_thread::sleep_for(timespan);
     }
 
     return 0;
