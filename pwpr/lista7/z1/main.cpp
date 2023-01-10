4ef324f5a3d53cbf0728d6a3d5cef3ab7529970d
Karol Makarowski <majkipll123@hotmail.com>
2023-01-10 21:08:55 +0100

    kulki

--- pwpr/lista7/z1/main.cpp
+++ pwpr/lista7/z1/main.cpp
@@ -1,30 +1,47 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-
     int szerokosc=800;
     int wysokosc=600;
     int promien=100;
     int keyonoff=1;
     int x=(szerokosc/2)-promien;
     int y=(wysokosc/2)-promien;
+    int d=2*promien;
     float vx=0.05;
     float vy=0.05;
-    //const int N=1000;
-    //sf::CiRcleShape kolka[N];
-    //void init()
-    //{
-        //for(int i=0;i<N;i++)
-        //{
-           // kolka[i] =sf::CircleShape(promien);
-            //kolka[i].setPosition(szerokosc/2+szerokosc/2*(rand()/(float)RAND_MAX))
-        //}
-    //}
+    const int N=10;
+    sf::CircleShape kolka[N];
+
+
+    void init()
+    {
+        for(int i=0;i<N;i++)
+        {
+            kolka[i] =sf::CircleShape(promien);
+            kolka[i].setPosition(x,y);
+            kolka[i].setFillColor(sf::Color::Green);
+        }
+    }
+
+void przesun(float Px, float Py,sf::CircleShape &obiektprzes)
+{
+    obiektprzes.move(Px,Py);
+        if (obiektprzes.getPosition().x>szerokosc)
+                      obiektprzes.setPosition(0,y);
+}
+
+int main()
+{
+
+
+
     sf::RenderWindow window(sf::VideoMode(szerokosc,wysokosc),"okno");
-    sf::CircleShape shape(promien);
-    shape.setFillColor(sf::Color::Black);
+
+    //sf::CircleShape shape(promien);
+
+    //shape.setFillColor(sf::Color::Black);//koloruje kolo
+    //shape.setPosition(x,y);
     while (window.isOpen())
     {
         sf::Event event;
@@ -38,10 +55,17 @@ int main()
                 keyonoff=1-keyonoff;
 
         }
-        shape.setPosition(x,y);
+        //for(int i=0;i<N;i++)
+          //  przesun(1.1,0,kolka[i]);
+
         window.clear(sf::Color::Blue);
         if(keyonoff)
-        window.draw(shape);
+        //{
+            for(int i=0;i<N;i++)
+            {
+                window.draw(kolka[i]);
+            }
+        //}
         window.display();
     }
     return 0;
--- pwpr/lista7/z1/test/main.cpp
+++ pwpr/lista7/z1/test/main.cpp
@@ -1,41 +1,70 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-int szerokosc=800;
-int wysokosc=600;
-int promien=100;
-const int N=100;
-sf::CircleShape kolka[N];
-void przesun(float PX, float PY, sf::CircleShape &obiektprzes)
-{
-    //przesunecie
-    obiektprzes.move(PX,PY);
-    if(obiektprzes.getPosition().x>wysokosc)
-        obiektprzes.setPosition(0,wysokosc/2-szerokosc);
-}
-void init()//tworzy kolka
+#include <iostream>
+    const int N=10;
+    int szerokosc=800;
+    int wysokosc=600;
+    int promien=100;
+    int keyonoff=1;
+    float g=9.81;
+    int R[N];
+    int G[N];
+    int B[N];
+    int x=(szerokosc/2)-promien;
+    int y=(wysokosc/2)-promien;
+    int d=2*promien;
+    float vx=1;
+    float vy=1;
+    float dx[N];
+    float dy[N];
+    int np[N];
+    //float pr[N];
+    sf::CircleShape kolka[N];
+
+
+    void init()
     {
         for(int i=0;i<N;i++)
         {
-            kolka[i] =sf::CircleShape (promien*(rand()/(float)RAND_MAX));
-            kolka[i].setPosition(szerokosc/2+szerokosc/2*(rand()/(float)RAND_MAX),
-                                       wysokosc/2+wysokosc/2*(rand()/(float)RAND_MAX));//x i y
-            kolka[i].setFillColor(sf::Color::Black);
+          //  for(int t=1;t<=N;t++){
+            np[i]=promien*(rand()/(float)RAND_MAX);
+            dx[i]=vx;//*g*t;
+            dy[i]=vy;//*g*t;
+            kolka[i] =sf::CircleShape(np[i]);
+            kolka[i].setPosition(x+x*(rand()/(float)RAND_MAX),y+y*(rand()/(float)RAND_MAX));
+            R[i]=200*(rand()/(float)RAND_MAX);
+            G[i]=200*(rand()/(float)RAND_MAX);
+            B[i]=200*(rand()/(float)RAND_MAX);
+            kolka[i].setFillColor(sf::Color(R[i],B[i],G[i]));
+           // pr[i]=1/i;
+          //  }
         }
     }
+
+void przesun(float &Px, float &Py,sf::CircleShape &przes,int nb)
+{
+        //Py=Py+vx*prs;
+        //std::cout<<przes.getPosition().x<<std::endl;
+        int l= przes.getPosition().x;
+        int k= przes.getPosition().y;
+        if (l+(2*nb)==szerokosc || l == 0)
+                      Px=Px*(-1); // juz jest wszystko tak
+        if (k+(2*nb)==wysokosc || k == 0)
+                      Py=Py*(-1);
+        przes.move(Px,Py);
+}
+
 int main()
 {
 
-
-    int keyonoff=1;
-    int x=(szerokosc/2)-promien;
-    int y=(wysokosc/2)-promien;
-    float vx=0.05;
-    float vy=0.05;
-
+    init();
 
     sf::RenderWindow window(sf::VideoMode(szerokosc,wysokosc),"okno");
+
     //sf::CircleShape shape(promien);
-    //shape.setFillColor(sf::Color::Black);
+
+    //shape.setFillColor(sf::Color::Black);//koloruje kolo
+    //shape.setPosition(x,y);
     while (window.isOpen())
     {
         sf::Event event;
@@ -49,13 +78,17 @@ int main()
                 keyonoff=1-keyonoff;
 
         }
-        //shape.setPosition(x,y);
-        for (int i=0; i<N; i++)
-            window.draw(kolka[i]);
-            //przesun(4.1,9,kolka[i]);
-        window.clear(sf::Color::Blue);
-        if(keyonoff)
+        for(int i=0;i<N;i++)
+            przesun(dx[i],dy[i],kolka[i],np[i]);
 
+        window.clear(sf::Color::Black);
+        if(keyonoff)
+        {
+            for(int i=0;i<N;i++)
+            {
+                window.draw(kolka[i]);
+            }
+        }
         window.display();
     }
     return 0;
